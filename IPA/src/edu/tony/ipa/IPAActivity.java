bbe3370ed8b27f9f52557fcb15263032b256e639
Weiru Dai <dweiru@gmail.com>
2011-10-24 10:23:21 +0800

    test java

--- IPA/src/edu/tony/ipa/IPAActivity.java
+++ IPA/src/edu/tony/ipa/IPAActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class IPAActivity extends Activity {
     /** Called when the activity is first created. */
+	//test
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
