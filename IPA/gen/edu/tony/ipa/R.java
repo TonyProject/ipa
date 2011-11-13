c2a2c9cefad92663155644fd934436be3529657c
Weiru Dai <dweiru@gmail.com>
2011-11-13 16:13:36 +0800

    add DB.java and empty ipachan.java to IPA

--- IPA/gen/edu/tony/ipa/R.java
+++ IPA/gen/edu/tony/ipa/R.java
@@ -13,8 +13,16 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int closet=0x7f050002;
+        public static final int clothes=0x7f050003;
+        public static final int clothes_type=0x7f050000;
+        public static final int ipachan=0x7f050004;
+        public static final int type=0x7f050001;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int ipa_closet=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
