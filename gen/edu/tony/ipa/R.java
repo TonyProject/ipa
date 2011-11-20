ac7fb16007e345f1e0ea8fcdfd31daa7039e5bb6
Weiru Dai <dweiru@gmail.com>
2011-11-20 13:32:15 +0800

    ipa charactor update

--- gen/edu/tony/ipa/R.java
+++ gen/edu/tony/ipa/R.java
@@ -15,14 +15,17 @@ public final class R {
     }
     public static final class id {
         public static final int closet=0x7f050002;
-        public static final int clothes=0x7f050003;
-        public static final int clothes_type=0x7f050000;
-        public static final int ipachan=0x7f050004;
-        public static final int type=0x7f050001;
+        public static final int gallery1=0x7f050005;
+        public static final int honor=0x7f050004;
+        public static final int ipachan=0x7f050000;
+        public static final int relativeLayout1=0x7f050001;
+        public static final int six=0x7f050003;
     }
     public static final class layout {
-        public static final int ipa_closet=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int ipa_chan=0x7f030000;
+        public static final int ipa_closet=0x7f030001;
+        public static final int ipa_honor=0x7f030002;
+        public static final int main=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/edu/tony/ipa/IPAChan.java
+++ src/edu/tony/ipa/IPAChan.java
@@ -1,5 +1,10 @@
 package edu.tony.ipa;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import org.apache.http.NameValuePair;
@@ -8,27 +13,61 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class IPAChan extends Activity {
+public class IPAChan extends Activity { 
+    private static final String IMG_URL= "http://4.bp.blogspot.com/_9joATD9m9xM/SAhKG9aeu6I/AAAAAAAAADQ/5h2tP-nco1o/s320/cute-emo-13.jpg";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.ipa_chan);
     	DB db = new DB();
     	ArrayList<NameValuePair> ipa = new ArrayList<NameValuePair>();
-    		ipa.add(new BasicNameValuePair("AccID","irene3119"));
-    		ArrayList<JSONObject> result_a = db.DataSearch(ipa,"ipa_search");
-    		Log.i("TEST", "go go go"+result_a.toString());
-    		for(int i=0;i<result_a.size();i++){
-    			try {
-	                Log.i("r_act",result_a.get(i).getString("ipaID"));
-                } catch (JSONException e) {
-	                // TODO Auto-generated catch block
-	                e.printStackTrace();
-                }
-    		}
+    	ipa.add(new BasicNameValuePair("AccID","irene3119"));
+    	ArrayList<JSONObject> result_a = db.DataSearch(ipa,"ipa_search");
+    	Log.i("TEST", "go go go"+result_a.toString());
+    	for(int i=0;i<result_a.size();i++){
+    		try {
+	            Log.i("r_act",result_a.get(i).getString("ipaID"));
+            } catch (JSONException e) {
+	          // TODO Auto-generated catch block
+	          e.printStackTrace();
+            }
+    	}
+        Button closet = (Button) findViewById(R.id.closet);
+        Button ipa_char = (Button) findViewById(R.id.six);
+        Button honor = (Button) findViewById(R.id.honor);
+    	ImageView imageView = (ImageView) findViewById(R.id.ipachan); 
+        Bitmap bitmap = getBitmapFromUrl(IMG_URL);
+        imageView.setImageBitmap(bitmap);
+    	
     }
+
+    /**
+     * 到Url去下載圖片回傳BITMAP回來
+     * @param imgUrl
+     * @return
+     */
+    private Bitmap getBitmapFromUrl(String imgUrl) {
+                URL url;
+                Bitmap bitmap = null;
+                try {
+                        url = new URL(IMG_URL);
+                        InputStream is = url.openConnection().getInputStream();
+                        BufferedInputStream bis = new BufferedInputStream(is);
+                        bitmap = BitmapFactory.decodeStream(bis);
+                        bis.close();
+                } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+                return bitmap;
+        }
 }
